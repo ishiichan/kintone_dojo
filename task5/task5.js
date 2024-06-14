20a8f6ac1099d456eac2e7e75bb3ac0061359f84
ishiichan <yuto-ishii@cybozu.co.jp>
2024-06-14 15:11:05 +0900

    seventh commit task5

--- task5/task5.js
+++ task5/task5.js
@@ -1,5 +1,54 @@
 'use strict';
 
+// 日付をセルに入れる関数
+const createDateCell = (i, eachRecordVal, day, cell) => {
+  document.getElementById(i + 'cell' + 0).textContent = eachRecordVal.day.value;
+  cell.className = 'cell1';
+};
+
+// カテゴリーをセルに入れる関数
+const createCategoryCell = (i, eachRecordVal, category, cell) => {
+  document.getElementById(i + 'cell' + 1).textContent =
+    eachRecordVal.category.value;
+  switch (eachRecordVal.category.value) {
+    case '企業情報':
+      cell.className = 'companyInformation';
+      break;
+    case 'IR 情報':
+      cell.className = 'irInformation';
+      break;
+    case '製品':
+      cell.className = 'product';
+      break;
+  }
+};
+
+// 記事内容をセルに入れる関数
+const createContentCell = (i, eachRecordVal, content, cell) => {
+  const page = document.createElement('a');
+  cell.appendChild(page);
+  page.id = cell.id;
+  cell.id = '';
+  document.getElementById(i + 'cell' + 2).textContent = editUrlText(
+    eachRecordVal.content.value
+  );
+  page.href = eachRecordVal.url.value;
+  // カテゴリーによって同じウィンドウか、別ウィンドウか使い分けるif文
+  if (eachRecordVal.target.value === '_self') {
+    page.target = '_self';
+  } else {
+    page.target = '_blank';
+  }
+};
+
+// 文字数カウント関数
+const editUrlText = (string) => {
+  if (string.length > 30) {
+    return string.substr(0, 30) + '...';
+  } else {
+    return string;
+  }
+};
 //   ↓ axiosによるAPIの取得
 const apiUrl =
   'https://oykh3vmu623yt5rufrstzlyxyi0kitod.lambda-url.ap-northeast-1.on.aws/';
@@ -8,7 +57,7 @@ axios
   .get(apiUrl, {
     params: {
       id: 'dojo',
-      query: 'limit 3',
+      query: 'order by day desc limit 3',
     },
   })
   .then((resp) => {
@@ -16,7 +65,7 @@ axios
     for (let index1 = 0; index1 < resp.data.length; index1++) {
       const record = document.createElement('tr');
       let eachVal = resp.data[index1];
-
+      console.log(eachVal);
       tables.appendChild(record);
       for (let index2 = 0; index2 < 3; index2++) {
         const cell = document.createElement('td');
@@ -24,53 +73,13 @@ axios
         cell.id = index1 + 'cell' + index2;
         record.appendChild(cell);
 
-        // 日付をセルに入れる関数
-        const createDateCell = (i) => {
-          document.getElementById(i + 'cell' + 0).textContent =
-            eachVal.day.value;
-          cell.className = 'cell1';
-        };
-
-        // カテゴリーをセルに入れる関数
-        const createCategoryCell = (i) => {
-          document.getElementById(i + 'cell' + 1).textContent =
-            eachVal.category.value;
-          switch (eachVal.category.value) {
-            case '企業情報':
-              cell.className = 'companyInformation';
-              break;
-            case 'IR 情報':
-              cell.className = 'irInformation';
-              break;
-            case '製品':
-              cell.className = 'product';
-              break;
-          }
-        };
-
-        // 記事内容をセルに入れる関数
-        const createContentCell = (i) => {
-          const page = document.createElement('a');
-          cell.appendChild(page);
-          page.id = cell.id;
-          cell.id = '';
-          document.getElementById(i + 'cell' + 2).textContent =
-            eachVal.content.value;
-          page.href = eachVal.url.value;
-          // カテゴリーによって同じウィンドウか、別ウィンドウか使い分けるif文
-          if (eachVal.category.value === '製品') {
-            page.target = '_self';
-          } else {
-            page.target = '_blank';
-          }
-        };
         // 短くなったそれぞれの列に値を挿入するif文
         if (index2 === 0) {
-          createDateCell(index1);
+          createDateCell(index1, eachVal, eachVal.day, cell);
         } else if (index2 === 1) {
-          createCategoryCell(index1);
+          createCategoryCell(index1, eachVal, eachVal.category, cell);
         } else {
-          createContentCell(index1);
+          createContentCell(index1, eachVal, eachVal.content, cell);
         }
       }
     }
