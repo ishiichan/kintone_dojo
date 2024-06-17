e52d0fa5c1a5423f33543c83a2801acc47404851
ishiichan <yuto-ishii@cybozu.co.jp>
2024-06-17 10:52:42 +0900

    second commit task6

--- kintone-jsapi-one.js
+++ kintone-jsapi-one.js
@@ -1,38 +1,37 @@
 (() => {
-  "use strict";
-  const events = ["app.record.create.show"];
+  'use strict';
+  const events = ['app.record.create.show'];
 
   kintone.events.on(events, (event) => {
-    const newRow1 = addRow("あくなき探求", "未振り返り");
-    const newRow2 = addRow("不屈の心体", "未振り返り");
-    const newRow3 = addRow("理想への共感", "未振り返り");
-    const newRow4 = addRow("心を動かす", "未振り返り");
-    const newRow5 = addRow("知識を増やす", "未振り返り");
-    const newRow6 = addRow("公明正大", "未振り返り");
-    event.record.Table.value = [
-      newRow1,
-      newRow2,
-      newRow3,
-      newRow4,
-      newRow5,
-      newRow6,
+    const newRow = [
+      'あくなき探求',
+      '不屈の心体',
+      '理想への共感',
+      '心を動かす',
+      '知識を増やす',
+      '公明正大',
     ];
+    event.record.Table.value.pop();
+    newRow.forEach((eachAction) => {
+      event.record.Table.value.push(addRow(eachAction));
+    });
+
     return event;
   });
 
-  const addRow = (action, situation) => ({
+  const addRow = (action) => ({
     value: {
       Action5: {
-        type: "DROP_DOWN",
+        type: 'DROP_DOWN',
         value: action,
       },
       課題: {
-        type: "MULTI_LINE_TEXT",
-        value: "",
+        type: 'MULTI_LINE_TEXT',
+        value: '',
       },
       状況: {
-        type: "CHECK_BOX",
-        value: [situation],
+        type: 'CHECK_BOX',
+        value: ['未振り返り'],
       },
     },
   });
