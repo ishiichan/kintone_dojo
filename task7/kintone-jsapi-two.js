f21fc29c1293e94c83484e4230641e44537cb64f
ishiichan <yuto-ishii@cybozu.co.jp>
2024-06-24 17:50:05 +0900

    second commit task7

--- task7/kintone-jsapi-two.js
+++ task7/kintone-jsapi-two.js
@@ -5,25 +5,18 @@
     "app.record.edit.show",
     "app.record.edit.submit",
     "app.record.create.submit",
+    "app.record.create.change.重複禁止項目_自動計算",
+    "app.record.edit.change.重複禁止項目_自動計算",
   ];
 
-  kintone.events.on(events, async (event) => {
-    event.record.重複禁止項目_文字列 = event.record.重複禁止項目_自動計算;
+  kintone.events.on(events, (event) => {
+    const day = dateFns.format(new Date(), "yyyyMMdd");
+    event.record.重複禁止項目_文字列.value = `${day}-${event.record.重複禁止項目_自動計算.value.substr(
+      9,
+      2
+    )}-${event.record.重複禁止項目_自動計算.value.substr(12, 1)}`;
     event.record.重複禁止項目_文字列.disabled = true;
 
-    const client = new KintoneRestAPIClient();
-
-    const res = await client.record.getAllRecords({
-      app: kintone.app.getId(),
-    });
-
-    console.log(res);
-    res.forEach((resp) => {
-      if (resp.重複禁止項目_文字列 === event.record.重複禁止項目_文字列) {
-        event.record.重複禁止項目_文字列.error =
-          "値がほかのレコードと重複しています。";
-      }
-    });
     return event;
   });
 })();
